f31b8108e19158ab944ebc520f874cf82c652808
sahua3228 <60621275+sahua3228@users.noreply.github.com>
2020-09-12 16:51:22 +0530

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -6,23 +6,36 @@ By considering the terms in the Fibonacci sequence whose values do not exceed fo
 
 public class Problem2 {
 
-	public static void main(String[] args) {
-		int n=4000000;
-		int a=0;
-		int b=1;
-		int sum=0;
-		int c=0;
-		for(;c<n;c++)
-		{
-			c=a+b;
-			a=b;
-			b=c;
-			if(c%2==0) {
-				sum=sum+c;
-			}
-		}
-System.out.println("Sum="+sum);
+	public static int series(int n)
+{
+	if(n==0)
+		return 1;
+	else if(n==1)
+		return 2;
+	else
+		return series(n-1)+series(n-2);
+	
+}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+     
+      int sum=0,n,r;
+      n=sc.nextInt();
+     for(int i=0;i<n;i++)
+     {
+    	 r=series(i);
+    	 if(r%2==0)
+    	 {
+    		 sum+=r;
+    	 }
+    	 System.out.print(r+"  ");
+     }
+     System.out.println();
+     System.out.println("sum="+sum);
 	}
 
 }
+		
 //Output=4613732
